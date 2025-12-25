4c5db26d5c9c1ac6cf1826e49f27f9ccceb4f5a0
sAfinAinUr <ainur2003g@gmail.com>
2025-12-25 03:59:58 +0300

    fix IconButton

--- src/ui/IconButton/IconButton.jsx
+++ src/ui/IconButton/IconButton.jsx
@@ -1,22 +1,8 @@
 import styles from './IconButton.module.scss';
 
 export default function IconButton({ children, className, ...props }) {
-  let additionallStyle;
-  switch (className) {
-    case 'delete':
-      additionallStyle = styles.delete;
-      break;
-    case 'ok':
-      additionallStyle = styles.ok;
-      break;
-    case 'close':
-      additionallStyle = styles.close;
-      break;
-  }
   return (
-    <button
-      className={className ? styles.Iconbutton + ' ' + additionallStyle : styles.Iconbutton}
-      {...props}>
+    <button className={`${styles.Iconbutton} ${styles[className] || ''}`} {...props}>
       {children}
     </button>
   );
