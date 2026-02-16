10234ff7cdd623106316d3be1cef7a1c53dcc0b0
sAfinAinUr <ainur2003g@gmail.com>
2026-02-16 18:01:19 +0300

    fix

--- src/components/PageMenu.tsx
+++ src/components/PageMenu.tsx
@@ -1,7 +1,7 @@
 import type { MenuProps } from 'antd';
 import { Menu } from 'antd';
 import MenuItem from 'antd/es/menu/MenuItem';
-import { useNavigate } from 'react-router';
+import { useNavigate, useLocation } from 'react-router';
 
 type MenuItem = Required<MenuProps>['items'][number];
 enum MenuItemKey {
@@ -18,11 +18,11 @@ const items: MenuItem[] = [
     ],
   },
 ];
-
+const defaultPage = [MenuItemKey.list];
 export default function PageMenu() {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
   const onClick: MenuProps['onClick'] = ({ key }) => {
-    console.log('click ', key);
     if (key === MenuItemKey.profile) {
       navigate('/profile');
     } else if (key === MenuItemKey.list) {
@@ -31,6 +31,13 @@ export default function PageMenu() {
   };
 
   return (
-    <Menu onClick={onClick} style={{ width: '100%', height: '100%' }} mode="inline" items={items} />
+    <Menu
+      selectedKeys={[pathname === '/' ? MenuItemKey.list : MenuItemKey.profile]}
+      defaultSelectedKeys={defaultPage}
+      onClick={onClick}
+      style={{ width: '100%', height: '100%' }}
+      mode="inline"
+      items={items}
+    />
   );
 }
